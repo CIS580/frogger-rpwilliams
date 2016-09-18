899edb7968019e5b37c1ca49e2352a2b333e3ec3
Ryan Williams <rpwilliams96@gmail.com>
2016-09-18 00:33:49 -0500

    Added frogger movement with INCORRECT animation

--- src/player.js
+++ src/player.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const MS_PER_FRAME = 1000/8;
+const JUMP_DISTANCE = 50;
 
 /**
  * @module exports the Player class
@@ -22,7 +23,56 @@ function Player(position) {
   this.spritesheet.src = encodeURI('assets/PlayerSprite2.png');
   this.timer = 0;
   this.frame = 0;
+
+  // Frog movement based on key commands
+ 
+    var self = this;
+    window.onkeydown = function(event)
+    {
+      event.preventDefault();
+      switch (event.keyCode)
+      {
+        // RIGHT
+        case 39:
+        case 68:
+          if(self.state == "idle") {
+            console.log("hop right");
+            self.state = "hop-1";
+            //self.x += JUMP_DISTANCE;
+          }
+          break;
+        // LEFT
+        case 37:
+        case 65:
+          if(self.state == "idle") {
+            console.log("hop left");
+            self.x -= JUMP_DISTANCE;
+            //self.state = "hop-1";
+          }
+          break;
+        // UP
+        case 38:
+        case 87:
+          if(self.state == "idle") {
+            console.log("hop up");
+            self.y -= JUMP_DISTANCE;
+            self.state = "hop-1";
+          }
+          break;
+        // DOWN
+        case 40:
+        case 83:
+          if(self.state == "idle") {
+            console.log("hop down");
+            self.y += JUMP_DISTANCE;
+            self.state = "hop-1";
+          }
+          break;
+      }
+    }
+  
 }
+
 
 /**
  * @function updates the player object
@@ -38,6 +88,18 @@ Player.prototype.update = function(time) {
         if(this.frame > 3) this.frame = 0;
       }
       break;
+    case "hop-1":
+      this.timer += time;
+      if(this.timer > MS_PER_FRAME) {
+
+        this.timer = 0;
+        this.frame += 1;
+        if(this.frame > 3) this.frame = 0;
+      }
+      //this.move(time);
+      break;
+
+
     // TODO: Implement your player's update by state
   }
 }
@@ -54,10 +116,22 @@ Player.prototype.render = function(time, ctx) {
         // image
         this.spritesheet,
         // source rectangle
-        this.frame * 64, 64, this.width, this.height,
+        this.frame * 64, 0*64, this.width, this.height,
         // destination rectangle
         this.x, this.y, this.width, this.height
       );
+      break;
+    case "hop-1":
+        // image
+        this.spritesheet,
+        // source rectangle
+        this.frame * 64, 1*64, this.width, this.height,
+        // destination rectangle
+        this.x, this.y, this.width, this.height
+      break;
+   
+      //this.x += JUMP_DISTANCE;
+      //this.state = "idle";
       break;
     // TODO: Implement your player's redering according to state
   }
